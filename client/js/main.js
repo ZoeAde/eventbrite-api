b05b1f9d29d07160d675cfc364e11e17d0560ba3
ZoeAde <zoe.a.adelman@gmail.com>
2016-06-13 16:12:37 -0600

    adding hide/show for buttons

--- client/js/main.js
+++ client/js/main.js
@@ -1,26 +1,20 @@
 $(document).on('ready', function() {
   console.log('sanity check!');
-        // $.get('http://localhost:5000/eventbrite/events').then(function (response) {
-        //     console.log('YO YO YO');
-        //    var events = response;
-        //             if(events.length) {
-        //                 var s;
-        //                 for(var i=0;i<events.length;i++) {
+        // HIDE SECTIONS ON LOAD
+        $('#eventData').hide();
+        $('#attendeeData').hide();
 
-        //                     var event = events[i];
-        //                     console.dir(event);
-        //                     s += "<tr class='eventList'>";
-        //                     s += "<td>" + event.id + "</td>";
-        //                     s += "<td>" + event.name + "</td>";
-        //                     s += "<td>" + event.description + "</td>";
-        //                     s += "</tr>";
-        //                     $('.event-table').append(s);
-        //                 }
+        // Show Sections on button click
+        $('.event-button-click').on('click', function() {
+            $('#attendeeData').hide();
+            $('#eventData').show();
+        });
 
-        //             } else {
-        //                 $events.html("<p>Sorry, there are no events.</p>");
-        //             }
-        // });
+        $('.attendee-button-click').on('click', function() {
+            $('#eventData').hide();
+            $('#attendeeData').show();
+        });
+
 
 
         $.get('http://localhost:5000/eventbrite/attendees').then(function (response) {
