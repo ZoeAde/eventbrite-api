b03da58212e9e3a00cc0e03c6ea057fb06a6508a
ZoeAde <zoe.a.adelman@gmail.com>
2016-06-16 11:10:03 -0600

    adding universal toggle checkbox selection

--- client/js/main.js
+++ client/js/main.js
@@ -1,21 +1,5 @@
 $(document).on('ready', function() {
   console.log('sanity check!');
-        // HIDE SECTIONS ON LOAD
-        $('#eventData').hide();
-        // $('#attendeeData').hide();
-
-        // Show Sections on button click
-        $('.event-button-click').on('click', function() {
-            $('#attendeeData').hide();
-            $('#eventData').show();
-        });
-
-        $('.attendee-button-click').on('click', function() {
-            $('#eventData').hide();
-            $('#attendeeData').show();
-        });
-
-
 
         //RADIO BUTTON SHOW/HIDE TABLE COLUMNS
         $("input:checkbox:not(:checked)").each(function() {
@@ -30,6 +14,8 @@ $(document).on('ready', function() {
 
         $("#checkAll").change(function () {
             $("input:checkbox").prop('checked', $(this).prop("checked"));
+            $('.col').toggle();
+            $('.colData').toggle();
         });
 
 
@@ -44,51 +30,51 @@ $(document).on('ready', function() {
                         for(var i=0;i<attendees.length;i++) {
                             var attendee = attendees[i];
                             console.log(attendee.profile_first_name);
-                            var newAttendee = "<tr class='attendeeList'><td class='col1'>" +
-                                attendee.team + "</td><td class='col2'>" +
-                                attendee.costs_base_price_display  + "</td><td class='col3'>" +
-                                attendee.costs_base_price_currency + "</td><td class='col4'>" +
-                                attendee.costs_base_price_value + "</td><td class='col5'>" +
-                                attendee.costs_base_price_major_value + "</td><td class='col6'>" +
-                                attendee.costs_eventbrite_fee_display + "</td><td class='col7'>" +
-                                attendee.costs_eventbrite_fee_currency + "</td><td class='col8'>" +
-                                attendee.costs_eventbrite_fee_value + "</td><td class='col9'>" +
-                                attendee.costs_eventbrite_fee_major_value + "</td><td class='col10'>" +
-                                attendee.costs_gross_display + "</td><td class='col11'>" +
-                                attendee.costs_gross_currency + "</td><td class='col12'>" +
-                                attendee.costs_gross_value + "</td><td class='col13'>" +
-                                attendee.costs_gross_major_value + "</td><td class='col14'>" +
-                                attendee.costs_payment_fee_display + "</td><td class='col15'>" +
-                                attendee.costs_payment_fee_currency + "</td><td class='col16'>" +
-                                attendee.costs_payment_fee_value + "</td><td class='col17'>" +
-                                attendee.costs_payment_fee_major_value + "</td><td class='col18'>" +
-                                attendee.costs_tax_display + "</td><td class='col19'>" +
-                                attendee.costs_tax_currency + "</td><td class='col20'>" +
-                                attendee.costs_tax_value + "</td><td class='col21'>" +
-                                attendee.costs_tax_major_value + "</td><td class='col22'>" +
-                                attendee.resource_uri + "</td><td class='col23'>" +
-                                attendee.id + "</td><td class='col24'>" +
-                                attendee.changed + "</td><td class='col25'>" +
-                                attendee.created + "</td><td class='col26'>" +
-                                attendee.quantity + "</td><td class='col27'>" +
-                                attendee.profile_first_name + "</td><td class='col28'>" +
-                                attendee.profile_last_name + "</td><td class='col29'>" +
-                                attendee.profile_email + "</td><td class='col30'>" +
-                                attendee.profile_name + "</td><td class='col31'>" +
-                                attendee.profile_addresses + "</td><td class='col32'>" +
-                                attendee.barcodes_status + "</td><td class='col33'>" +
-                                attendee.barcodes_barcode + "</td><td class='col34'>" +
-                                attendee.barcodes_checkin_type + "</td><td class='col35'>" +
-                                attendee.barcodes_created + "</td><td class='col36'>" +
-                                attendee.barcodes_changed + "</td><td class='col37'>" +
-                                attendee.answers + "</td><td class='col38'>" +
-                                attendee.checked_in + "</td><td class='col39'>" +
-                                attendee.cancelled + "</td><td class='col40'>" +
-                                attendee.refunded + "</td><td class='col41'>" +
-                                attendee.affiliate + "</td><td class='col42'>" +
-                                attendee.status + "</td><td class='col43'>" +
-                                attendee.event_id + "</td><td class='col44'>" +
-                                attendee.order_id + "</td><td class='col45'>" +
+                            var newAttendee = "<tr class='attendeeList'><td class='colData col1'>" +
+                                attendee.team + "</td><td class='colData col2'>" +
+                                attendee.costs_base_price_display  + "</td><td class='colData col3'>" +
+                                attendee.costs_base_price_currency + "</td><td class='colData col4'>" +
+                                attendee.costs_base_price_value + "</td><td class='colData col5'>" +
+                                attendee.costs_base_price_major_value + "</td><td class='colData col6'>" +
+                                attendee.costs_eventbrite_fee_display + "</td><td class='colData col7'>" +
+                                attendee.costs_eventbrite_fee_currency + "</td><td class='colData col8'>" +
+                                attendee.costs_eventbrite_fee_value + "</td><td class='colData col9'>" +
+                                attendee.costs_eventbrite_fee_major_value + "</td><td class='colData col10'>" +
+                                attendee.costs_gross_display + "</td><td class='colData col11'>" +
+                                attendee.costs_gross_currency + "</td><td class='colData col12'>" +
+                                attendee.costs_gross_value + "</td><td class='colData col13'>" +
+                                attendee.costs_gross_major_value + "</td><td class='colData col14'>" +
+                                attendee.costs_payment_fee_display + "</td><td class='colData col15'>" +
+                                attendee.costs_payment_fee_currency + "</td><td class='colData col16'>" +
+                                attendee.costs_payment_fee_value + "</td><td class='colData col17'>" +
+                                attendee.costs_payment_fee_major_value + "</td><td class='colData col18'>" +
+                                attendee.costs_tax_display + "</td><td class='colData col19'>" +
+                                attendee.costs_tax_currency + "</td><td class='colData col20'>" +
+                                attendee.costs_tax_value + "</td><td class='colData col21'>" +
+                                attendee.costs_tax_major_value + "</td><td class='colData col22'>" +
+                                attendee.resource_uri + "</td><td class='colData col23'>" +
+                                attendee.id + "</td><td class='colData col24'>" +
+                                attendee.changed + "</td><td class='colData col25'>" +
+                                attendee.created + "</td><td class='colData col26'>" +
+                                attendee.quantity + "</td><td class='colData col27'>" +
+                                attendee.profile_first_name + "</td><td class='colData col28'>" +
+                                attendee.profile_last_name + "</td><td class='colData col29'>" +
+                                attendee.profile_email + "</td><td class='colData col30'>" +
+                                attendee.profile_name + "</td><td class='colData col31'>" +
+                                attendee.profile_addresses + "</td><td class='colData col32'>" +
+                                attendee.barcodes_status + "</td><td class='colData col33'>" +
+                                attendee.barcodes_barcode + "</td><td class='colData col34'>" +
+                                attendee.barcodes_checkin_type + "</td><td class='colData col35'>" +
+                                attendee.barcodes_created + "</td><td class='colData col36'>" +
+                                attendee.barcodes_changed + "</td><td class='colData col37'>" +
+                                attendee.answers + "</td><td class='colData col38'>" +
+                                attendee.checked_in + "</td><td class='colData col39'>" +
+                                attendee.cancelled + "</td><td class='colData col40'>" +
+                                attendee.refunded + "</td><td class='colData col41'>" +
+                                attendee.affiliate + "</td><td class='colData col42'>" +
+                                attendee.status + "</td><td class='colData col43'>" +
+                                attendee.event_id + "</td><td class='colData col44'>" +
+                                attendee.order_id + "</td><td class='colData col45'>" +
                                 attendee.ticket_class_id + "</td></tr>";
                             $('.attendee-table').append(newAttendee);
                         }
